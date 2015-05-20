5c050eb99c8bc4702e4e7782cb9c2e686a0febb9
Ogamedia <kwame.yeboah@meltwater.org>
2015-05-20 21:48:18 +0000

    needs intense styling

---
+++ hired.js
@@ -0,0 +1,20 @@
+// if (Meteor.isClient) {
+//   // counter starts at 0
+//   Session.setDefault('counter', 0);
+
+//   Template.hello.helpers({
+//     counter: function () {
+//       return Session.get('counter');
+//     }
+//   });
+
+// }
+
+// if (Meteor.isServer) {
+//   Meteor.startup(function () {
+//     // code to run on server at startup
+//   });
+
+
+
+// }
