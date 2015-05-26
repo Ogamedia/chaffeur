b791a0eff33a788094d409c2fc9e34536a08677c
Ogamedia <kwame.yeboah@meltwater.org>
2015-05-26 11:42:30 +0000

    working on hooks

--- client/js/hooks.js
+++ client/js/hooks.js
@@ -20,23 +20,74 @@
 
  			// price calculation
  			var price = 0;
- 				// based on location"Mini-Bus", "Sedan", "SUV"
+ 			// based on location
+ 				// location within accra
  				if (pickLocation == "Accra International Airport (Kotoka)" || "Accra") {
- 					if (carType == "Sedan", "SUV") {
- 						price = 80;
+ 					// based on range of car type
+ 					if (range == "luxury") {
+ 						// luxury pricing
+ 						if (carType == "Sedan") {
+ 							price = 120;
+ 						} else if(carType == "SUV"){
+ 							price = 150;
+ 						}
+ 						else if(carType == "Mini-Bus"){
+ 							price = 200;
+ 						} else {
+ 							price = 120;
+ 						}
+ 					} else {
+ 						// economy and midrange pricing
+ 						if (carType == "Sedan") {
+ 							price = 80;
+ 						} else if(carType == "SUV"){
+ 							price = 140;
+ 						}
+ 						else if(carType == "Mini-Bus"){
+ 							price = 150;
+ 						} else {
+ 							price = 100;
+ 						}
+ 					};
+ 					// based on range of car type
+ 				} else if (pickLocation == "Outside Accra") {
+ 						// luxury pricing
+ 						if (range == "luxury") {
+ 							if (carType == "Sedan") {
+ 								price = 150;
+ 							} else if(carType == "SUV"){
+ 								price = 200;
+ 							}
+ 							else if(carType == "Mini-Bus"){
+ 								price = 200;
+ 							} else {
+ 								price = 150;
+ 							}
+ 						// economy and midrange pricing
+ 					} else{
+ 						if (carType == "Sedan") {
+ 							price = 120;
+ 						} else if(carType == "SUV"){
+ 							price = 160;
+ 						}
+ 						else if(carType == "Mini-Bus"){
+ 							price = 170;
+ 						} else {
+ 							price = 120;
+ 						}
+ 					}
+ 				} else if (pickLocation == "Other") {
+ 					// other location prices are standardised price
+ 					if (carType == "Sedan") {
+ 						price = 150;
  					} else if(carType == "SUV"){
- 						price = 140;
+ 						price = 180;
  					}
  					else if(carType == "Mini-Bus"){
- 						price = 150;
+ 						price = 200;
  					} else {
- 						price = 100;
+ 						price = 120;
  					}
- 				} else if (pickLocation == "Accra International Airport (Kotoka)" || "Accra") {
-
- 				} else {
-
- 					// based on car type
  				}
 
 
@@ -51,8 +102,8 @@
  				car_range:range,
  				car_type: carType,
  				phone_number: phone,
- 				email_address: emailAddress
-
+ 				email_address: emailAddress,
+ 				pricing: price
  			}
 
  			var html=Blaze.toHTMLWithData(Template.mailContent, dataContext);
